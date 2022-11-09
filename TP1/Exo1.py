3c14e3da15c73c84988fbeb4456630af0c7e70b9
panama04 <kenan.meylan@gmail.com>
2022-11-09 16:58:53 +0100

    first commit tp1

---
+++ TP1/Exo1.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class PremierProgramme(App):
+    def build(self):
+        return Label(text='Bonjour, je suis étudiant en R&T', font_size='50')
+
+if __name__ == '__main__':
+    try:
+        PremierProgramme().run()
+    except KeyboardInterrupt:
+        PremierProgramme().stop()
